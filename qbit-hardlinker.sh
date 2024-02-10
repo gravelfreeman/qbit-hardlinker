98d006f226ade7446a6beee67b37fb56a1b7f599
bankaihide <157883895+bankaihide@users.noreply.github.com>
2024-02-10 17:19:31 +0100

    Update qbit-hardlinker.sh
    Handled torrent with a category associated

--- qbit-hardlinker.sh
+++ qbit-hardlinker.sh
@@ -3,17 +3,20 @@
 # Get the download path
 download_path="$1"
 
-# This is the base path that qbittorent downloads into
-base_path="/media/qbittorrent/data"
+#get category
+category="$2"
 
-# The destination folder 
+# This is the base path that qbittorent downloads into
+base_path="/data/torrents/completed"
+
+# The destination folder
 #
 # This is where we want our hardlinks to end up, must be on the same filesystem.
-destination="/media/qbittorrent/done"
+destination="/data/torrents/hardlinksync"
 
 # Optional logging
 enable_logging=true
-log_file="/media/sdh1/dgibbons/qbit-linker.log"
+log_file="/config/filebot/qbit-linker.log"
 
 # Optional empty directory cleanup
 enable_cleanup=false
@@ -27,6 +30,9 @@ fi
 
 # Create destination directory if it doesn't exist
 mkdir -p "$destination"
+
+# Create category directory if it doesn't exist
+mkdir -p "$destination/$category"
 
 # Function to create a hard link
 create_hardlink() {
@@ -46,6 +52,10 @@ find "$download_path" -type d | while read -r dir; do
     # Extract relative path
     relative_path="${dir#"$base_path"/}"
     dir_path="$destination/$relative_path"
+    if [[ -n "$category" ]]; then
+        dir_path="$destination/$category/$relative_path"
+    fi
+
     mkdir -p "$dir_path"
     [ "$enable_logging" = true ] && echo "Created directory structure: $dir_path" >> "$log_file"
 done
@@ -53,9 +63,11 @@ done
 find "$download_path" -type f | while read -r file; do
     # Extract relative path
     relative_path="${file#"$base_path"/}"
-
-    # Create hard link
-    create_hardlink "$file" "$destination/$relative_path"
+    dest_path="$destination/$relative_path"
+    if [[ -n "$category" ]]; then
+        dest_path="$destination/$category/$relative_path"
+    fi
+    create_hardlink "$file" "$dest_path"
 done
 
 # Clean up empty folders from previous runs
